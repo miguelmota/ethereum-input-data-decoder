1e90fadb8ed062e793d9038a7ca40bc3fdbfd193
Miguel Mota <miguelmota2@gmail.com>
2021-03-14 14:53:33 -0700

    wip

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const fs = require('fs')
-const ethabi = require('ethereumjs-abi')
 const ethers = require('ethers')
-const Buffer = require('buffer/').Buffer
 const isBuffer = require('is-buffer')
 
 class InputDataDecoder {
@@ -35,9 +33,8 @@ class InputDataDecoder {
         continue
       }
 
-      const method = obj.name || null
+      const method = obj.name || 'constructor'
       const types = obj.inputs ? obj.inputs.map(x => x.type) : []
-      const names = obj.inputs ? obj.inputs.map(x => x.name) : []
 
       // take last 32 bytes
       data = data.slice(-256)
@@ -50,13 +47,12 @@ class InputDataDecoder {
         data = `0x${data}`
       }
 
-      const inputs = ethers.utils.defaultAbiCoder.decode(types, data)
+      const result = ethers.utils.defaultAbiCoder.decode(types, data)
+      const inputs = formatInputs(result, obj.inputs)
 
       return {
         method,
-        types,
-        inputs,
-        names
+        inputs
       }
     }
 
@@ -74,194 +70,77 @@ class InputDataDecoder {
 
     data = data.trim()
 
-    const dataBuf = Buffer.from(data.replace(/^0x/, ''), 'hex')
-    const methodId = toHexString(dataBuf.subarray(0, 4))
-    var inputsBuf = dataBuf.subarray(4)
+    for (var i = 0; i < this.abi.length; i++) {
+      const obj = this.abi[i]
 
-    const result = this.abi.reduce((acc, obj) => {
-      if (obj.type === 'constructor') return acc
-      if (obj.type === 'event') return acc
-      const method = obj.name || null
-      let types = obj.inputs ? obj.inputs.map(x => {
-        if (x.type.includes('tuple')) {
-          return x
-        } else {
-          return x.type
-        }
-      }) : []
+      if (obj.type === 'constructor') {
+        continue
+      }
 
-      let names = obj.inputs ? obj.inputs.map(x => {
-        if (x.type.includes('tuple')) {
-          return [x.name, x.components.map(a => a.name)]
-        } else {
-          return x.name
-        }
-      }) : []
+      if (obj.type === 'event') {
+        continue
+      }
 
-      const hash = genMethodId(method, types)
-
-      if (hash === methodId) {
-        let inputs = []
-
-        try {
-          inputsBuf = normalizeAddresses(types, inputsBuf)
-          inputs = ethabi.rawDecode(types, inputsBuf)
-        } catch (err) {
-          inputs = ethers.utils.defaultAbiCoder.decode(types, inputsBuf)
-          // defaultAbiCoder attaches some unwanted properties to the list object
-          inputs = deepRemoveUnwantedArrayProperties(inputs)
-
-          // TODO: do this normalization into normalizeAddresses
-          inputs = inputs.map((input, i) => {
-            if (types[i].components) {
-              const tupleTypes = types[i].components
-              return deepStripTupleAddresses(input, tupleTypes)
-            }
-            if (types[i] === 'address') {
-              return input.split('0x')[1]
-            }
-            if (types[i] === 'address[]') {
-              return input.map(address => address.split('0x')[1])
-            }
-            return input
-          })
-        }
-
-        // Map any tuple types into arrays
-        const typesToReturn = types.map(t => {
-          if (t.components) {
-            const arr = t.components.reduce((acc, cur) => [...acc, cur.type], [])
-            const tupleStr = `(${arr.join(',')})`
-            if (t.type === 'tuple[]') return tupleStr + '[]'
-            return tupleStr
-          }
-          return t
-        })
+      try {
+        const method = obj.name
+        const ifc = new ethers.utils.Interface([])
+        const result = ifc.decodeFunctionData(ethers.utils.FunctionFragment.fromObject(obj), data)
+console.log("INPUT", result)
+        const inputs = formatInputs(result, obj.inputs)
 
         return {
           method,
-          types: typesToReturn,
-          inputs,
-          names
+          inputs
         }
-      }
-
-      return acc
-    }, { method: null, types: [], inputs: [], names: [] })
-
-    if (!result.method) {
-      try {
-        const decoded = this.decodeConstructor(data)
-        if (decoded) {
-          return decoded
-        }
-      } catch (err) { }
+      } catch (err) {}
     }
 
-    return result
+    try {
+      const decoded = this.decodeConstructor(data)
+      if (decoded) {
+        return decoded
+      }
+    } catch (err) { }
+
+    return {
+      method: null,
+      inputs: []
+    }
   }
 }
 
-// remove 0x from addresses
-function deepStripTupleAddresses (input, tupleTypes) {
-  return input.map((item, i) => {
-    const type = tupleTypes[i].type
-    if (type === 'address' && typeof item === 'string') {
-      return item.split('0x')[1]
+function formatInputs(input, abi) {
+  let result = []
+  for (let i = 0; i < abi.length; i++) {
+    let value = null
+    try {
+      value = input[abi[i].name]
+    } catch(err) {
+      console.log(err.message)
     }
-    if (type === 'address[]' || Array.isArray()) {
-      return item.map(a => a.split('0x')[1])
-    }
-    return item
-  })
-}
-
-function deepRemoveUnwantedArrayProperties (arr) {
-  return [...arr.map(item => {
-    if (Array.isArray(item)) return deepRemoveUnwantedArrayProperties(item)
-    return item
-  })]
-}
-
-function normalizeAddresses (types, input) {
-  let offset = 0
-  for (let i = 0; i < types.length; i++) {
-    const type = types[i]
-    if (type === 'address') {
-      input.set(Buffer.alloc(12), offset)
-    }
-
-    if (isArray(type)) {
-      const size = parseTypeArray(type)
-      if (size && size !== 'dynamic') {
-        offset += 32 * size
+    if (abi[i].components) {
+      result.push({
+        name: abi[i].name,
+        type: abi[i].type,
+        value: formatInputs(value[0], abi[i].components)
+      })
+    } else {
+      if (Array.isArray(value)) {
+        result.push({
+          name: abi[i].name,
+          type: abi[i].type,
+          value: value
+        })
       } else {
-        offset += 32
+        result.push({
+          name: abi[i].name,
+          type: abi[i].type,
+          value: value.toString(),
+        })
       }
-    } else {
-      offset += 32
     }
   }
-
-  return input
-}
-
-function parseTypeArray (type) {
-  const tmp = type.match(/(.*)\[(.*?)\]$/)
-  if (tmp) {
-    return tmp[2] === '' ? 'dynamic' : parseInt(tmp[2], 10)
-  }
-  return null
-}
-
-function isArray (type) {
-  return type.lastIndexOf(']') === type.length - 1
-}
-
-function handleInputs (input, tupleArray) {
-  if (input instanceof Object && input.components) {
-    input = input.components
-  }
-
-  if (!Array.isArray(input)) {
-    if (input instanceof Object && input.type) {
-      return input.type
-    }
-
-    return input
-  }
-
-  let ret = '(' + input.reduce((acc, x) => {
-    if (x.type === 'tuple') {
-      acc.push(handleInputs(x.components))
-    } else if (x.type === 'tuple[]') {
-      acc.push(handleInputs(x.components) + '[]')
-    } else {
-      acc.push(x.type)
-    }
-    return acc
-  }, []).join(',') + ')'
-
-  if (tupleArray) {
-    return ret + '[]'
-  }
-
-  return ret
-}
-
-function genMethodId (methodName, types) {
-  const input = methodName + '(' + (types.reduce((acc, x) => {
-    acc.push(handleInputs(x, x.type === 'tuple[]'))
-    return acc
-  }, []).join(',')) + ')'
-
-  return ethers.utils.keccak256(Buffer.from(input)).slice(2, 10)
-}
-
-function toHexString (byteArray) {
-  return Array.from(byteArray, function (byte) {
-    return ('0' + (byte & 0xFF).toString(16)).slice(-2)
-  }).join('')
+  return result
 }
 
 module.exports = InputDataDecoder
--- test/test.test.js
+++ test/test.test.js
@@ -2,204 +2,167 @@ const fs = require('fs')
 const test = require('tape')
 const InputDataDecoder = require('../index')
 
-function hexify (input) {
-  if (Array.isArray(input)) {
-    return input.map(hexify)
-  }
+// https://etherscan.io/tx/0xa6f019f2fc916bd8df607f1c99148ebb06322999ff08bc88927fe8406acae1b2
+const data = fs.readFileSync(`${__dirname}/data/abi1_input_data.txt`)
 
-  if (input) {
-    if (input.toHexString) {
-      input = input.toHexString()
-    }
-  }
-
-  return input
-}
-
-test('decoder', t => {
-  // https://etherscan.io/tx/0xa6f019f2fc916bd8df607f1c99148ebb06322999ff08bc88927fe8406acae1b2
-  const data = fs.readFileSync(`${__dirname}/data/abi1_input_data.txt`)
-
-  /*
-  t.test('abi filepath', t => {
-    t.plan(6)
+describe('decoder', () => {
+  it('abi filepath', () => {
     const decoder = new InputDataDecoder(`${__dirname}/data/abi1.json`)
-
     const result = decoder.decodeData(data)
-    t.equal(result.method, 'registerOffChainDonation')
-    t.deepEqual(result.types, [
+    expect(result.method).toBe('registerOffChainDonation')
+    expect(result.inputs.map(x => x.type)).toStrictEqual([
       'address',
       'uint256',
       'uint256',
       'string',
       'bytes32'
     ])
-    t.equal(result.inputs[0].toString(), '0x5A9dAC9315FdD1c3D13eF8Af7FDFEB522Db08F02')
-    t.equal(result.inputs[1].toHexString(), '0x58a20230')
-    t.equal(result.inputs[2].toHexString(), '0x402934')
-    t.equal(result.inputs[3], 'BTC')
-    t.end()
+    expect(result.inputs[0].value).toBe('0x5A9dAC9315FdD1c3D13eF8Af7FDFEB522Db08F02')
+    expect(result.inputs[1].value).toBe('1487012400')
+    expect(result.inputs[2].value).toBe('4204852')
+    expect(result.inputs[3].value).toBe('BTC')
   })
-*/
 
-  /*
-  t.test('abi array object', t => {
-    t.plan(7)
+  it('abi array object', () => {
     const abi = JSON.parse(fs.readFileSync(`${__dirname}/data/abi1.json`))
     const decoder = new InputDataDecoder(abi)
-
     const result = decoder.decodeData(data)
-    t.equal(result.method, 'registerOffChainDonation')
-    t.deepEqual(result.types, ['address', 'uint256', 'uint256', 'string', 'bytes32'])
-    t.equal(result.inputs[0].toString(), '0x5A9dAC9315FdD1c3D13eF8Af7FDFEB522Db08F02')
-    t.equal(result.inputs[1].toHexString(), '0x58a20230')
-    t.equal(result.inputs[2].toHexString(), '0x402934')
-    t.equal(result.inputs[3], 'BTC')
-    t.deepEqual(result.names, ['addr', 'timestamp', 'chfCents', 'currency', 'memo'])
-    t.end()
+    expect(result.method).toBe('registerOffChainDonation')
+    expect(result.inputs.map(x => x.type)).toStrictEqual(['address', 'uint256', 'uint256', 'string', 'bytes32'])
+    expect(result.inputs[0].value).toBe('0x5A9dAC9315FdD1c3D13eF8Af7FDFEB522Db08F02')
+    expect(result.inputs[1].value).toBe('1487012400')
+    expect(result.inputs[2].value).toBe('4204852')
+    expect(result.inputs[3].value).toBe('BTC')
+    expect(result.inputs.map(x => x.name)).toStrictEqual(['addr', 'timestamp', 'chfCents', 'currency', 'memo'])
   })
-*/
 
-  /*
   // https://etherscan.io/tx/0xc7add41f6ae5e4fe268f654709f450983510ab7da67812be608faf2133a90b5a
-  t.test('contract creation data (constructor)', t => {
-    t.plan(4)
-
+  it.skip('contract creation data (constructor)', () => {
     const abi = JSON.parse(fs.readFileSync(`${__dirname}/data/abi1.json`))
     const decoder = new InputDataDecoder(abi)
-
     const data = fs.readFileSync(`${__dirname}/data/contract_creation_data.txt`)
     const result = decoder.decodeData(data)
-    t.equal(result.inputs[0].toString(), '0xB2Cb826C945D8Df01802b5cf3c4105685D4933A0')
-    t.equal(result.inputs[1].toString(), 'STIFTUNG Dfinity FDC')
-
-    t.equal(result.names[0].toString(), '_masterAuth')
-    t.equal(result.names[1].toString(), '_name')
+    expect(result.inputs[0].value).toBe('0xB2Cb826C945D8Df01802b5cf3c4105685D4933A0')
+    expect(result.inputs[1].value).toBe('STIFTUNG Dfinity FDC')
+    expect(result.inputs[0].name).toBe('_masterAuth')
+    expect(result.inputs[1].name).toBe('_name')
   })
-*/
 
-  // NOOOOOO
-  /*
   // https://etherscan.io/tx/0x94fadf5f5c7805b8ceb8a13a0a7fbce06054ff08cdfdc2fd555a7902592aebe6
-  t.test('erc721 transferFrom', t => {
-    t.plan(6)
-
+  it.skip('erc721 transferFrom', () => {
     const abi = JSON.parse(fs.readFileSync(`${__dirname}/data/erc721_abi.json`))
     const decoder = new InputDataDecoder(abi)
-
     const data = fs.readFileSync(`${__dirname}/data/erc721_transferfrom_tx_data.txt`)
     const result = decoder.decodeData(data)
-console.log("yo",result)
-
-    t.equal(result.inputs[0].toString(), '0x10017ca37B1257Ac0771e24652aa28c758e378Eb')
-    t.equal(result.inputs[1].toString(), 'e7a632d89104385bdd3992eeb82cffeb48e4e539')
-    t.equal(result.inputs[2].toString(), '5dc5')
-
-    t.equal(result.names[0], '_from')
-    t.equal(result.names[1], '_to')
-    t.equal(result.names[2], '_tokenId')
+    expect(result.inputs[0].value).toBe('0x10017ca37B1257Ac0771e24652aa28c758e378Eb')
+    expect(result.inputs[1].value).toBe('0xe7a632d89104385bdd3992eeb82cffeb48e4e539')
+    expect(result.inputs[2].value).toBe('24005')
+    expect(result.inputs[0].name).toBe('_from')
+    expect(result.inputs[1].name).toBe('_to')
+    expect(result.inputs[2].name).toBe('_tokenId')
   })
-*/
 
-  /*
   // marketSellOrders call
   // https://etherscan.io/tx/0xc79ee30142e935453eabd57f45e01bb394bff78d05cdf8df97631b03ad6cc0cd
-  t.test('0x exchange sell (abiv2 tuple[])', t => {
-    t.plan(4)
-
+  it('0x exchange sell (abiv2 tuple[])', () => {
     const abi = JSON.parse(fs.readFileSync(`${__dirname}/data/0x_exchange.json`))
     const decoder = new InputDataDecoder(abi)
-
     const data = fs.readFileSync(`${__dirname}/data/0x_exchange_data.txt`)
     const result = decoder.decodeData(data)
-
-    t.deepEqual(result.method, 'marketSellOrders')
-
+console.log("RESULT", JSON.stringify(result, null, 2))
+    expect(result.method).toBe('marketSellOrders')
     const expectedInputs =  [
-      [
-        [
-          "0x6f02E6d47147B4448Fe2f2eb25B4f534cf110c23",
-          "0x0000000000000000000000000000000000000000",
-          "0xA258b39954ceF5cB142fd567A46cDdB31a670124",
-          "0x0000000000000000000000000000000000000000",
-          "0x410d586a20a4bffff5",
-          "0x5e05647aedbbd450",
-          "0x00",
-          "0x00",
-          "0x5d787202",
-          "0x016d1e79ae50",
-          "0xf47261b000000000000000000000000089d24a6b4ccb1b6faa2625fe562bdd9a23260359",
-          "0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+      {
+name: 'orders',
+type: 'tuple[]',
+value: [
+          {
+        name: 'makerAddress',
+        type: 'address',
+        value: "0x6f02E6d47147B4448Fe2f2eb25B4f534cf110c23",
+          },
+          {
+        name: 'takerAddress',
+        type: 'address',
+        value: "0x0000000000000000000000000000000000000000",
+          },
+          {
+        name: 'feeRecipientAddress',
+        type: 'address',
+        value: "0xA258b39954ceF5cB142fd567A46cDdB31a670124",
+          },
+          {
+        name: 'senderAddress',
+        type: 'address',
+        value: "0x0000000000000000000000000000000000000000",
+          },
+          {
+        name: 'makerAssetAmount',
+        type: 'uint256',
+        value: "1199999999999999999989",
+          },
+          {
+        name: 'takerAssetAmount',
+        type: 'uint256',
+        value: "6774931693586076752",
+          },
+          {
+        name: 'makerFee',
+        type: 'uint256',
+        value: "0",
+          },
+          {
+        name: 'takerFee',
+        type: 'uint256',
+        value: "0",
+          },
+          {
+        name: 'expirationTimeSeconds',
+        type: 'uint256',
+        value: "1568174594",
+          },
+          {
+        name: 'salt',
+        type: 'uint256',
+        value: "1568174354000",
+          },
+          {
+        name: 'makerAssetData',
+        type: 'bytes',
+        value: "0xf47261b000000000000000000000000089d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+          },
+          {
+        name: 'takerAssetData',
+        type: 'bytes',
+        value: "0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+          },
         ]
-      ],
-      "0x2386f26fc10000",
-      [
-        "0x1b82e97aa18170e6b81ce3a829d77b7067cf3644c8706e97e7c96d5a92de61eb0c5c5aeb4fbfadca6b9fbc5adff91bfb32964aa9e1bf8309dad7e1bd3e45f0b44c03"
-      ]
+      },
+      {
+        name: 'takerAssetFillAmount',
+        type: 'uint256',
+        value: "10000000000000000",
+      },
+{
+name: 'signatures',
+value: [ "0x1b82e97aa18170e6b81ce3a829d77b7067cf3644c8706e97e7c96d5a92de61eb0c5c5aeb4fbfadca6b9fbc5adff91bfb32964aa9e1bf8309dad7e1bd3e45f0b44c03" ],
+type: 'bytes[]'
+}
     ]
 
-    t.deepEqual(hexify(result.inputs), expectedInputs)
-
-    const expectedTypes = [
-      [
-        [
-          'address',
-          'address',
-          'address',
-          'address',
-          'uint256',
-          'uint256',
-          'uint256',
-          'uint256',
-          'uint256',
-          'uint256',
-          'bytes',
-          'bytes'
-        ],
-      ],
-      'uint256',
-      'bytes[]'
-    ]
-
-    t.deepEqual(result.types, expectedTypes)
-
-    const expectedNames = [
-      [
-        'orders',
-        [
-          'makerAddress',
-          'takerAddress',
-          'feeRecipientAddress',
-          'senderAddress',
-          'makerAssetAmount',
-          'takerAssetAmount',
-          'makerFee',
-          'takerFee',
-          'expirationTimeSeconds',
-          'salt',
-          'makerAssetData',
-          'takerAssetData'
-        ]
-      ],
-      'takerAssetFillAmount',
-      'signatures'
-    ]
-
-    t.deepEqual(result.names, expectedNames)
+    expect(result.inputs).toStrictEqual(expectedInputs)
   })
-*/
 
   // https://etherscan.io/tx/0xcb0c447659123c5faa2f1e5bc8ac69697688f437c92a8abb4b882bb33cbc661a
-  t.test('set issuance (abiv2 tuple)', t => {
-    t.plan(4)
-
+  it.skip('set issuance (abiv2 tuple)', () => {
     const abi = JSON.parse(fs.readFileSync(`${__dirname}/data/set_exchange_issuance_lib.json`))
     const decoder = new InputDataDecoder(abi)
 
     const data = fs.readFileSync(`${__dirname}/data/set_issuance.txt`)
     const result = decoder.decodeData(data)
+console.log("RESULT", result)
 
-    t.equal(result.method, 'issueRebalancingSetWithEther')
+    expect(result.method).toBe('issueRebalancingSetWithEther')
 
     const expectedInputs = [
       '81c55017F7Ce6E72451cEd49FF7bAB1e3DF64d0C',
@@ -216,14 +179,14 @@ console.log("yo",result)
         ],
         [
           '0x06989640c83ea4a200',
-          '0x19c110'
+          '0x19c110',
         ]
       ],
       '0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000040400000000000000000000000000000000000000000000000000000000000000420000000000000000000000000000000000000000000000000d5a4cdef78c36901bc9ab7c3108c7d7b06f183e7d71c591bb6c677e3fa2958310a2520b916b792c6538b8e43ccb9a1773e94daa441307827c377f3b197d6549f9de54794a806b697a0300000000000000000000000056178a0d5f301baf6cf3e1cd53d9863437345bf90000000000000000000000000acd0b5cf881cd8398ac563872209de1ce15df0f00000000000000000000000055662e225a3376759c24331a9aed764f8f0c9fbb0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006b70202a2f0d520000000000000000000000000000000000000000000000000000d97e20f757ec00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005dfa648800000000000000000000000000000000000000000000000015e187ad8bbe980000000000000000000000000089d24a6b4ccb1b6faa2625fe562bdd9a23260359000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc200000000000000000000000000000000000000000000000000000000000000420000000000000000000000000000000000000000000000000caa536649a0fdc71bd88fc3a7ecf72f2082e39783296db7a0236b404ca968873f51ce89b4237ecdcf0da1d3cee4306348664629deacaca7f7961ada746036229c5b161612cdb96bad0300000000000000000000000056178a0d5f301baf6cf3e1cd53d9863437345bf90000000000000000000000000acd0b5cf881cd8398ac563872209de1ce15df0f00000000000000000000000055662e225a3376759c24331a9aed764f8f0c9fbb000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001a38050000000000000000000000000000000000000000000000000ce4d38704d0c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005dfa648800000000000000000000000000000000000000000000000015e187ad8bbe98000000000000000000000000002260fac5e5542a773aa44fbcfedf7c193bc2c599000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
       true
     ]
 
-    t.deepEqual(hexify(result.inputs), expectedInputs)
+    expect(result.inputs).toBe(expectedInputs)
 
     const expectedNames = [
       '_rebalancingSetAddress',
@@ -243,7 +206,7 @@ console.log("yo",result)
       '_orderData',
       '_keepChangeInVault'
     ]
-    t.deepEqual(result.names, expectedNames)
+    expect(result.names).toBe(expectedNames)
 
     const expectedTypes = [
       'address',
@@ -262,20 +225,17 @@ console.log("yo",result)
       'bytes',
       'bool'
     ]
-    t.deepEqual(result.types, expectedTypes)
+    expect(result.types).toBe(expectedTypes)
   })
 
-/*
   // https://github.com/miguelmota/ethereum-input-data-decoder/issues/8
-  t.test('256 address', t => {
-    t.plan(2)
+  it.only('256 address', () => {
     const decoder = new InputDataDecoder(`${__dirname}/data/abi2.json`)
-
     const data = '0xa9059cbb85f1150654584d0192059454e9dc1532d9d9cf914926406a02370cea80cf32f600000000000000000000000000000000000000000000000000000000033dc10b'
-
     const result = decoder.decodeData(data)
-    t.equal(result.inputs[0].toString(), 'e9dc1532d9d9cf914926406a02370cea80cf32f6')
-    t.equal(result.inputs[1].toString(), '54378763')
+console.log("RR", result)
+    expect(result.inputs[0].value).toBe('0xe9dc1532d9d9cf914926406a02370cea80cf32f6')
+    expect(result.inputs[1].value).toBe('54378763')
   })
-*/
 })
+
