eb5bc44ac59f536d8801e26aac77489c85b451e2
Alexander <wyemond@gmail.com>
2021-09-07 11:49:37 +0300

    Add real TypeScript definitions
    It was impossible to use the exported class as a type with
    just a stub-like module declaration

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,18 @@
-declare module "ethereum-input-data-decoder"
+import { AbiItem } from "web3-utils";
+
+type NestedArray<T> = T | NestedArray<T>[];
+
+export interface InputData {
+  method: string | null;
+  types: string[];
+  inputs: any[];
+  names: NestedArray<string>[];
+}
+
+export default class InputDataDecoder {
+  constructor(abi: string | AbiItem[]);
+
+  decodeConstructor(data: Buffer | string): InputData;
+
+  decodeData(data: Buffer | string): InputData;
+}
